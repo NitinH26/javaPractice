3ac71fd8567162541fb54abee36eef43535ddef7
hniti <hniti@LAPTOP-MGOFCHQS>
2023-04-20 20:27:27 +0530

    first commit with addition program

---
+++ src/simplePrograms/Addition.java
@@ -0,0 +1,29 @@
+package simplePrograms;
+
+public class Addition 
+{
+	 static int x;
+	
+	public static void main(String[] args) 
+	{
+		
+		add();
+
+	}
+	
+	static void add()
+	{
+		int a,b;
+		int sum;
+		
+		a=10;
+		b=20;
+		sum=a+b;
+		
+		System.out.println(sum);
+
+	}
+	
+
+	
+}
